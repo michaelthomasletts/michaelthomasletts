c762513b665860f8c278a6863e637007e99990e9
Mike Letts <michaelthomasletts@gmail.com>
2025-05-25 17:05:44 -0700

    .

---
+++ chart.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from pepy_chart import PepyStats
+
+
+if __name__ == "__main__":
+    PepyStats(
+        package="boto3-refresh-session",
+        output_path="downloads.png",
+        automatically_open_img=False,
+    )
